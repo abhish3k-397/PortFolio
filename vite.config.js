9f55f547333c58a00e0c3fc1870f328c6a757fb8
abhish3k-397 <abhishek246krishna@gmail.com>
2025-12-02 11:11:03 +0530

    chore: Update Vite base path and configure Vite development server host.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/PortFolio/',
+    base: '/',
+    server: {
+        host: '0.0.0.0'
+    }
 })
-// Deployment fix trigger
